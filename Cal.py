2ae63d1e66efa46229b8d4b26f112e7523ac8953
binu2002 <binojmadhuranga2002.04.04@gmail.com>
2025-10-21 07:22:18 +0530

    Remove unwanted lines

--- Cal.py
+++ Cal.py
@@ -1,24 +1,19 @@
-# Simple Python Calculator
 
-# Function to perform addition
 def add(x, y):
     return x + y
 
-# Function to perform subtraction
 def subtract(x, y):
     return x - y
 
-# Function to perform multiplication
 def multiply(x, y):
     return x * y
 
-# Function to perform division
 def divide(x, y):
     if y == 0:
         return "Error: Cannot divide by zero"
     return x / y
 
-# Main calculator loop
+
 while True:
     print("\nSimple Calculator")
     print("1. Addition (+)")
